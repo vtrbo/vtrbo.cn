74066e0b158c6f9d8115b254ebb2961bdfd0979a
vtrbo <vtrbo@users.noreply.github.com>
2023-05-10 10:33:43 +0000

    Release by 4b8de2709e7100e43f309e25c7014573d2e271eb

--- _app/immutable/chunks/stores.3d4e24d2.js
+++
@@ -1,1 +0,0 @@
-import"./index.d5715330.js";import{s as e}from"./singletons.bd1d3b6d.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
